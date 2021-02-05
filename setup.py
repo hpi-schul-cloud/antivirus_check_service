67070108f0423fe4c62f02e16bfa6c0c152d7f08
ralfnellescap <ralf.nelles.ext@capgemini.com>
2021-02-05 10:34:05 +0000

    [OPS-1722] fix double versioning, clean and fix workflow

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+path = './version'
+version_file = open(path,'r')
+get_version = version_file.read()
+version_file.close()
 
 setup(
     name='antivirus_service',
-    version='0.1.0',
+    version=get_version,
     description='This service detects virus in downloaded files by using clamd',
     author='matthias wiesner',
     packages=find_packages(exclude='tests'),
